7fcd24b48d952884b8a7b5ddbc83219358a1b3fd
greg-bahr <gregsbahr@gmail.com>
2021-12-05 23:59:57 -0600

    switch from bigints to longs

--- src/main/kotlin/dev/gregbahr/problems/Day6.kt
+++ src/main/kotlin/dev/gregbahr/problems/Day6.kt
@@ -1,26 +1,19 @@
 package dev.gregbahr.problems
 
-import java.math.BigInteger
-
 class Day6(private val data: String) {
 
-    fun solve(daysToSimulate: Int): BigInteger {
+    fun solve(daysToSimulate: Int): Long {
         val nums = data.split(",").map { it.toInt() }
-        val map = nums.groupingBy { it }.eachCount().mapValues { BigInteger.valueOf(it.value.toLong()) }.toMutableMap()
-            .withDefault { BigInteger.ZERO }.apply {
+        val map = nums.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap().withDefault { 0L }.apply {
             for (i in (1 until daysToSimulate)) {
                 val zeroes = getValue(i)
 
-                this[i] = BigInteger.ZERO
+                this[i] = 0L
                 this[i + 7] = getValue(i + 7) + zeroes
                 this[i + 9] = getValue(i + 9) + zeroes
             }
         }
 
         return map.values.sum()
-    }
-
-    private fun MutableCollection<BigInteger>.sum(): BigInteger {
-        return this.reduce { acc, bigInteger -> acc + bigInteger }
     }
 }
